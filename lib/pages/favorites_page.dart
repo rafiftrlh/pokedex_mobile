289bf92574740e524470632c0d9af08c08249cb1
rafiftrlh <rafifitro89@gmail.com>
2024-09-17 11:04:02 +0700

    feat(navigation): add navigation in list favorite;

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_mobile_new/models/pokemon.dart';
+import 'package:pokedex_mobile_new/pages/pokemon_detail_page.dart';
 
 class FavoritesPage extends StatelessWidget {
   final List<Pokemon> favoritePokemonList;
@@ -20,51 +21,61 @@ class FavoritesPage extends StatelessWidget {
               itemCount: favoritePokemonList.length,
               itemBuilder: (context, index) {
                 final pokemon = favoritePokemonList[index];
-                return Container(
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.only(bottom: 15),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 0,
-                          blurRadius: 2,
-                          offset: Offset(0, 1),
+                return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              PokemonDetailPage(pokemon: pokemon),
                         ),
-                      ]),
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 60,
-                        height: 60,
-                        child: ClipRRect(
+                      );
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.only(bottom: 15),
+                      decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
-                          child: Image.network(pokemon.image),
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.2),
+                              spreadRadius: 0,
+                              blurRadius: 2,
+                              offset: Offset(0, 1),
+                            ),
+                          ]),
+                      child: Row(
                         children: [
-                          Text(
-                            pokemon.name,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500),
+                          Container(
+                            width: 60,
+                            height: 60,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.network(pokemon.image),
+                            ),
                           ),
-                          SizedBox(height: 5),
-                          Text(
-                            pokemon.category,
-                            style: TextStyle(color: Colors.grey[500]),
+                          SizedBox(width: 10),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                pokemon.name,
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                pokemon.category,
+                                style: TextStyle(color: Colors.grey[500]),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
-                  ),
-                );
+                    ));
               },
             ),
     );
